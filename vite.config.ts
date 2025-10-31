ee43de746b0c6fdfafe02fd352494edf89e2d385
cursethesemetalhans <65410686+cursethesemetalhans@users.noreply.github.com>
2025-10-31 17:45:12 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
 
 {/*
